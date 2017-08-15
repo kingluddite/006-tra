bed4305e736a2513ba37473ba01bb37442dd8f47
Philip Howley <howley.phil@gmail.com>
2017-08-14 20:30:44 -0700

    complete save-lat-lng

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -20,15 +20,6 @@ exports.createStore = async (req, res) => {
   res.redirect(`/store/${store.slug}`);
 };
 
-exports.createStore = async (req, res) => {
-  const store = await new Store(req.body).save();
-  req.flash(
-    'success',
-    `Successfully Created ${store.name}. Care to leave a review?`,
-  );
-  res.redirect(`/store/${store.slug}`);
-};
-
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
