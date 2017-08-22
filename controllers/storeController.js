6bd22687ee1f75b8632dbd3e14d31e29e1c87c55
Philip Howley <howley.phil@gmail.com>
2017-08-21 18:29:18 -0700

    finish quick-data notes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -45,6 +45,7 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
   // find and update the store
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // returns the new store instead of the old one
