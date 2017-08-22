ac50064440bdeea396254a873e549962444adb4a
Philip Howley <howley.phil@gmail.com>
2017-08-21 20:58:27 -0700

    complete upload-resize notes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
 const Store = mongoose.model('Store');
+
+const multerOptions = {
+  // 1. Where will file be stored when it is uploaded?
+  storage: multer.memoryStorage(),
+  // 2. What types of files are allowed?
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That filetype isn't allowed" }, false);
+    }
+  },
+};
 
 exports.homePage = (req, res) => {
   console.log(req.name);
@@ -8,6 +26,24 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware
+    return; // make sure to leave the function
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to our filesystem, keep going!
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -45,7 +81,7 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
-  req.body.location.type = 'Point';
+  // req.body.location.type = 'Point';
   // find and update the store
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // returns the new store instead of the old one
--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -1,17 +1,17 @@
-function autocomplete(input, latInput, lngInput) {
-  if (!input) return; // skip this function from running if there is no address input
-  const dropdown = new google.maps.places.Autocomplete(input);
+// function autocomplete(input, latInput, lngInput) {
+//   if (!input) return; // skip this function from running if there is no address input
+//   const dropdown = new google.maps.places.Autocomplete(input);
 
-  dropdown.addListener('place_changed', () => {
-    const place = dropdown.getPlace();
-    console.log(place);
-    latInput.value = place.geometry.location.lat();
-    lngInput.value = place.geometry.location.lng();
-  });
-  // if someone hits enter on the address field, don't submit the form
-  input.on('keydown', evt => {
-    if (evt.keyCode === 13) evt.preventDefault();
-  });
-}
+//   dropdown.addListener('place_changed', () => {
+//     const place = dropdown.getPlace();
+//     console.log(place);
+//     latInput.value = place.geometry.location.lat();
+//     lngInput.value = place.geometry.location.lng();
+//   });
+//   // if someone hits enter on the address field, don't submit the form
+//   input.on('keydown', evt => {
+//     if (evt.keyCode === 13) evt.preventDefault();
+//   });
+// }
 
-export default autocomplete;
+// export default autocomplete;
